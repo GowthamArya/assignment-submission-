5daeb68de60a3eadd97066f2e12abd3d037386ce
GowthamArya <118643766+GowthamArya@users.noreply.github.com>
2022-11-20 08:05:52 +0530

    Update 5primenos.cpp

--- 5primenos.cpp
+++ 5primenos.cpp
@@ -1,18 +1,15 @@
 #include <iostream> 
 int main(){
-	int ct=0,n=0,i=1,j=1;  
-    while(n<5)  
-    {  
+    int ct=0,n=0,i=1,j=1;  
+    while(n<5){  
         j=1;  
         ct=0;  
-        while(j<=i)  
-        {  
+        while(j<=i){  
             if(i%j==0)  
             ct++;  
             j++;   
         }  
-        if(ct==2)  
-        {  
+        if(ct==2){  
             printf("%d ",i);  
             n++;  
         }  
